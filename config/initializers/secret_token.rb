a362da1c174fd878cf9fae219132c010cc8556bf
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-11-10 13:56:56 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zapire::Application.config.secret_token = '724f0ad3606fe3646d760179fca3a7f2c81aa27092efb27cd20db03ad834c7cbfec6e425d1413dee0f8462692c485491b7c9c02557977d801fdc7f29a0a002de'
